4c05da7357da9627289261ff02a711f944e69663
Adeade151592 <adeade1592@gmail.com>
2025-12-18 15:08:46 +0000

    fix: update Kubernetes version from 1.28 to 1.30
    - Kubernetes 1.28 AMIs are no longer supported
    - Update to 1.30 for latest stable release
    - Fixes InvalidParameterException for node group creation

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -18,7 +18,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.30"
 }
 
 variable "enable_public_access" {
